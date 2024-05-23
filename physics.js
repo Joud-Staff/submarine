6e55e6b3ee27456b386b1024107a7ccc4b42a442
AshrafEtmeh <ashraftmh1@gmail.com>
2024-05-23 08:24:30 +0300

    split

---
+++ physics.js
@@ -0,0 +1,17 @@
+class Submarine {
+    class Front_wings {
+        // joud
+
+        // end joud
+    }
+    class Back_wings {
+        // Thaer
+
+        //end Thaer
+    }
+    class Rudder {
+        //Sera
+
+        //end Sera
+    }
+}
